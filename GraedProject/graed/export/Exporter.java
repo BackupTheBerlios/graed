8652e7f2227d42433a49b8ffdbd6a58aca29d568
dusk93 <dusk93>
2005-03-04 09:15:16 +0000

    *** empty log message ***

--- GraedProject/graed/export/Exporter.java
+++ GraedProject/graed/export/Exporter.java
@@ -5,9 +5,13 @@ package graed.export;
 
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.PrintJob;
 import java.awt.image.BufferedImage;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JFrame;
 
 import com.sun.image.codec.jpeg.ImageFormatException;
 
@@ -37,15 +41,33 @@ public class Exporter {
         out.close();
     }
     
+    public static void exportToPrinter( JFrame jf, Component c ) {
+        Properties props = new Properties();
+    	
+    	props.setProperty("awt.print.paperSize", "A4");
+    	props.setProperty("awt.print.destination", "printer");
+    	    	
+    	PrintJob job = jf.getToolkit().getPrintJob
+    	    (jf, "Impression", props);
+    	if (job != null) {
+    	    Graphics g = job.getGraphics();
+    	    
+    	    jf.printAll(g);
+    	    g.dispose();
+    	    job.end();
+    	}
+    }
+    
     public static void main( String[] args ) throws ImageFormatException, IOException {
         javax.swing.JFrame f = new javax.swing.JFrame();
         javax.swing.JButton test = new javax.swing.JButton("Test export JPEG");
         f.getContentPane().add( test);
         f.pack();
         f.setVisible(true);
-        javax.swing.JFileChooser jfc = new javax.swing.JFileChooser();
+        /*javax.swing.JFileChooser jfc = new javax.swing.JFileChooser();
         int state = jfc.showSaveDialog(null);
         f.dispose();
-        Exporter.exportToJpeg( f.getContentPane(), jfc.getSelectedFile().getCanonicalPath() );
+        Exporter.exportToJpeg( f.getContentPane(), jfc.getSelectedFile().getCanonicalPath() );*/
+        Exporter.exportToPrinter(f,test);
     }
 }
